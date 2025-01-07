c0e248898f0aa429669e6bdfe4cf97c7de8eae3d
Pedro Hugo Rodrigues Barbosa <40366968+Pedrohugovm@users.noreply.github.com>
2025-01-07 02:03:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Olá, mundo! Esta é a página inicial do projeto.</h1>
+  return <h1>Olá, mundo! Esta é a página inicial do projeto.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
