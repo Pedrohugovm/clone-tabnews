3cbd89755e986601a2202062382173dc136b1d4a
Pedro Hugo Rodrigues Barbosa <40366968+Pedrohugovm@users.noreply.github.com>
2025-03-04 02:37:40 +0000

    evita que métodos diferentes de POST ou GET sejam enviados para o /migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,57 +3,48 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  let databaseOpenedConnections = await getDatabaseOpenedConnections();
-  console.log(`Conexões abertas no início: ${databaseOpenedConnections}`);
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  try {
+    const allowedMethods = ["GET", "POST"];
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    databaseOpenedConnections = await getDatabaseOpenedConnections();
-    console.log(`Conexões abertas GET no final: ${databaseOpenedConnections}`);
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-
-    await dbClient.end();
-    databaseOpenedConnections = await getDatabaseOpenedConnections();
-    console.log(`Conexões abertas POST no final: ${databaseOpenedConnections}`);
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    if (!allowedMethods.includes(request.method)) {
+      console.log("Método não permitido");
+      return response.status(405).end();
     }
 
-    return response.status(200).json(migratedMigrations);
+    const dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      await dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await dbClient.end();
+    return response.status(405).end();
   }
-
-  databaseOpenedConnections = await getDatabaseOpenedConnections();
-  console.log(`Conexões abertas no final: ${databaseOpenedConnections}`);
-
-  return response.status(405).end();
-}
-
-async function getDatabaseOpenedConnections() {
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
-
-  return databaseOpenedConnectionsValue;
 }
